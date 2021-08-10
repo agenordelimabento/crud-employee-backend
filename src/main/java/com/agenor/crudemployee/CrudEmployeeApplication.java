2b596bab0f2986b5e3957e68dc8832e507b360cb
Agenor de Lima Bento <agenordelima@msn.com>
2021-08-10 15:43:58 -0300

    Project created

---
+++ src/main/java/com/agenor/crudemployee/CrudEmployeeApplication.java
@@ -0,0 +1,13 @@
+package com.agenor.crudemployee;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrudEmployeeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrudEmployeeApplication.class, args);
+	}
+
+}
